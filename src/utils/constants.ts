81ed15114920325f2d3a4443f4bbbd4369fa6018
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 16:24:15 +0100

    feat: add network constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,6 @@
 // Application constants
 
 export const APP_NAME = 'Clarity xBTC Send Plus';
+
+export const NETWORK = 'mainnet';
+export const CONTRACT_ADDRESS = 'SP3DX3H4FEYZJZ586MFBS25ZW3HZDMEW92260R2PR';
