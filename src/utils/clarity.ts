c7d4d43b88b2fa76ecb51ef2977901a6afa30819
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 16:32:41 +0100

    feat: add recipient tuple builder

--- src/utils/clarity.ts
+++ src/utils/clarity.ts
@@ -15,3 +15,15 @@ export const toMemo = (memo: string) => {
     const encoder = new TextEncoder();
     return bufferCV(encoder.encode(memo.slice(0, 34)));
 };
+
+export const buildRecipientTuple = (
+    to: string,
+    amount: number,
+    memo: string
+) => tupleCV({
+    to: toPrincipal(to),
+    'xbtc-in-sats': toUint(amount),
+    memo: toMemo(memo),
+    'swap-to-ustx': uintCV(0),
+    'min-dy': uintCV(0)
+});
