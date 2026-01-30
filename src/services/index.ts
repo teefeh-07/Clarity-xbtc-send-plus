f3fb2f319228d1b7b62b98b03194e4edf0db22ed
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 16:33:15 +0100

    refactor: update services index exports

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,8 @@
-// Service layer exports
-
-export {};
+// Service exports
+export * from './stacks';
+export * from './transactions';
+export * from './api';
+export * from './wallet';
+export * from './balance';
+export * from './prices';
+export * from './history';
