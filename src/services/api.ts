cdf1da0c7b1561efdb3d49bc5def7c3cb582dd58
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 16:24:59 +0100

    feat: add address transactions API

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,3 +6,8 @@ export const fetchTransaction = async (txId: string) => {
     const response = await fetch(`${API_BASE}/extended/v1/tx/${txId}`);
     return response.json();
 };
+
+export const fetchAddressTransactions = async (address: string, limit: number = 20) => {
+    const response = await fetch(`${API_BASE}/extended/v1/address/${address}/transactions?limit=${limit}`);
+    return response.json();
+};
