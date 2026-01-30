201f6016325aadb7284d1a862623649114e76990
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 16:32:36 +0100

    feat: add memo conversion utility

--- src/utils/clarity.ts
+++ src/utils/clarity.ts
@@ -10,3 +10,8 @@ import {
 
 export const toUint = (value: number) => uintCV(value);
 export const toPrincipal = (address: string) => principalCV(address);
+
+export const toMemo = (memo: string) => {
+    const encoder = new TextEncoder();
+    return bufferCV(encoder.encode(memo.slice(0, 34)));
+};
