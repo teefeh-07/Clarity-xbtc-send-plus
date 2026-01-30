3836e0739fef6f2daea2616456002a1e31d26cd9
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 16:24:17 +0100

    feat: add transaction limit constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,3 +4,6 @@ export const APP_NAME = 'Clarity xBTC Send Plus';
 
 export const NETWORK = 'mainnet';
 export const CONTRACT_ADDRESS = 'SP3DX3H4FEYZJZ586MFBS25ZW3HZDMEW92260R2PR';
+
+export const MAX_RECIPIENTS = 200;
+export const MAX_MEMO_LENGTH = 34;
