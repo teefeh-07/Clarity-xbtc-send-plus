d32f9d7ecfc5ad8837508797fb0210758db64177
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 16:40:18 +0100

    refactor: consolidate utility exports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,11 @@
-// Utility functions
-
-export {};
+// Utility exports
+export * from './format';
+export * from './validation';
+export * from './constants';
+export * from './crypto';
+export * from './storage';
+export * from './api';
+export * from './clarity';
+export * from './errors';
+export * from './logger';
+export * from './numbers';
