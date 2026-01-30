6dd52d431c9be50d791f9a3e09b93e6fe6169ba1
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 16:32:46 +0100

    feat: add wallet connect function

--- src/services/wallet.ts
+++ src/services/wallet.ts
@@ -5,3 +5,18 @@ import { AppConfig, showConnect, UserSession } from '@stacks/connect';
 const appConfig = new AppConfig(['store_write', 'publish_data']);
 
 export const createUserSession = () => new UserSession({ appConfig });
+
+export const connectWallet = (
+    userSession: UserSession,
+    onSuccess: (data: any) => void
+) => {
+    showConnect({
+        appDetails: {
+            name: 'xBTC Send Plus',
+            icon: window.location.origin + '/icon.png',
+        },
+        redirectTo: '/',
+        onFinish: onSuccess,
+        userSession,
+    });
+};
