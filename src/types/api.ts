15f8378b181901942954feb75165b093bad49620
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 16:23:52 +0100

    feat: add paginated response type

--- src/types/api.ts
+++ src/types/api.ts
@@ -5,3 +5,9 @@ export interface ApiResponse<T> {
     data?: T;
     error?: string;
 }
+
+export interface PaginatedResponse<T> extends ApiResponse<T[]> {
+    page: number;
+    totalPages: number;
+    totalItems: number;
+}
