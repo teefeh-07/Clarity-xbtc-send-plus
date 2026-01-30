d6738b2b7656e482f567a6eddec85cc87768c32c
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 16:24:00 +0100

    feat: add amount formatting utility

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -4,3 +4,7 @@ export const formatAddress = (address: string): string => {
     if (!address) return '';
     return `${address.slice(0, 6)}...${address.slice(-4)}`;
 };
+
+export const formatAmount = (amount: number, decimals: number = 8): string => {
+    return (amount / Math.pow(10, decimals)).toFixed(decimals);
+};
