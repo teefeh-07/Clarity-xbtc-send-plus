de52a7698daa4c19746f4fad03e6173b18149388
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 16:37:59 +0100

    feat: add context barrel export

---
+++ src/context/index.ts
@@ -0,0 +1,4 @@
+export { StacksProvider, useStacks } from './StacksContext';
+export { ThemeProvider, useTheme } from './ThemeContext';
+export { NetworkProvider, useNetwork } from './NetworkContext';
+export { ToastProvider, useToast } from './ToastContext';
