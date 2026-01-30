1933a9d699162151d7faf32d79f8a99ef6cbebd9
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 16:24:30 +0100

    feat: create reusable Input component

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface InputProps {
+    value: string;
+    onChange: (value: string) => void;
+    placeholder?: string;
+    type?: string;
+}
+
+export const Input: React.FC<InputProps> = ({ value, onChange, placeholder, type = 'text' }) => {
+    return (
+        <input 
+            type={type}
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+            placeholder={placeholder}
+            className="input"
+        />
+    );
+};
