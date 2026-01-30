f3b0fa7ffad2726e33ab9936cee01896729803f6
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 16:24:52 +0100

    feat: add balance fetching service

--- src/services/stacks.ts
+++ src/services/stacks.ts
@@ -5,3 +5,8 @@ import { StacksMainnet, StacksTestnet } from '@stacks/network';
 export const getNetwork = (isMainnet: boolean = true) => {
     return isMainnet ? new StacksMainnet() : new StacksTestnet();
 };
+
+export const fetchAccountBalance = async (address: string) => {
+    const response = await fetch(`https://api.mainnet.hiro.so/extended/v1/address/${address}/balances`);
+    return response.json();
+};
