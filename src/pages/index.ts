aa82f0d7b4cbfa69e73760c746fe3c28e57b2dc7
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 16:37:38 +0100

    feat: add pages barrel export

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { HomePage } from './HomePage';
+export { TransferPage } from './TransferPage';
+export { BatchPage } from './BatchPage';
+export { HistoryPage } from './HistoryPage';
+export { SettingsPage } from './SettingsPage';
