bb0ec0983881bd174c4738b2429a16fbc008526a
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 16:32:12 +0100

    feat: add crypto utility for memo hashing

---
+++ src/utils/crypto.ts
@@ -0,0 +1,9 @@
+// Crypto utilities
+
+export const hashMemo = async (memo: string): Promise<string> => {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(memo);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+};
